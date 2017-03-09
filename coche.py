119fca8aa65bd38327c7961240bbb9270390f1bb
adriarr <adriarr@l118c3-linux.lab.inf.uva.es>
2017-03-09 17:34:13 +0100

    Write/Read records

--- coche.py
+++ coche.py
@@ -4,9 +4,9 @@ class Coche:
     def __init__(self,load_data):
         """ Parsea los datos de cada coche según el formato del fichero e inicializa sus variables internas """
         self.orientation = load_data[0]
-        self.x = load_data[1]
-        self.y = load_data[2]
-        self.size = load_data[3]
+        self.x = int(load_data[1])
+        self.y = int(load_data[2])
+        self.size = int(load_data[3])
     def mover(self,direction):
         if self.orientation == "H":
             self.x = self.x + direction
--- main.py
+++ main.py
@@ -8,14 +8,17 @@ def play_menu():
 
     return None
 
-def see_records():
-    """ Muestra los récords actualmente guardados y ofrece una opción para resetearlos"""
-
-    return None
-
-
-
-
+def write_records():
+	f_records = open("records.txt","w")
+	for record in records:
+		f_records.write(str(record))
+		f_records.write("\n")
+def read_records():
+	f_records = open("records.txt","r")
+	for line in f_records:
+		records.append(int(line))
+	print records		
+		
 
 print "Anrokku"
 print "="*80
@@ -23,26 +26,22 @@ print "Arroyo Calle, Adrián"
 print "Crespo Jiménez, Cristina Alejandra"
 print ""
 
-exit = False
+records = []
+read_records()
 
-while not exit:
-    print "MENÚ PRINCIPAL"
-    print "1. Jugar"
-    print "2. Ver récords"
-    print "0. Salir"
-    
-    option = -1
-    while option < 0 or option > 2:
-        option = raw_input("Selecciona la opción: ")
-        try:
-            option = int(option)
-        except:
-            pass
-    if option == 1:
-        play_menu()
-    elif option == 2:
-        see_records()
-    else:
-        exit = True
+
+niveles = []
+f_niveles = open("niveles.txt","r")
+n_niveles = f_niveles.readline()
+n_niveles = int(n_niveles)
+
+for i in range(0,n_niveles):
+	n_coches = int(f_niveles.readline())
+	coches = []
+	for j in range(0,n_coches):
+		coches.append(coche.Coche(f_niveles.readline()))
+	niveles.append(coches[:])
+
+play_menu()
 
 print "Gracias por jugar"
