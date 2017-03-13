f3d8fa03adb87b9acaaff2c7bba3978eaadc56bb
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2017-03-13 21:52:16 +0100

    Colisiones

--- coche.py
+++ coche.py
@@ -1,12 +1,41 @@
 # -*- coding:utf-8 -*-
 
 class Coche:
-    def __init__(self,load_data):
+    def __init__(self,load_data,name):
         """ Parsea los datos de cada coche según el formato del fichero e inicializa sus variables internas """
+        self.name = name
         self.orientation = load_data[0]
         self.x = int(load_data[1])
         self.y = int(load_data[2])
         self.size = int(load_data[3])
+    def libre(self,x,y):
+        if self.orientation == "H":
+            if x >= self.x and x < self.x + self.size:
+                if y == self.y:
+                    return False
+        else:
+            if y >= self.y and y < self.y + self.size:
+                if x == self.x:
+                    return False
+        return True
+    def casilla_libre(self,direction,nivel):
+        x = self.x
+        y = self.y
+        if self.orientation == "H":
+            x += self.size if direction > 0 else direction
+        else:
+            y += self.size if direction > 0 else direction
+        libre = True
+        for car in nivel:
+            if car.name != self.name:
+                libre = libre and car.libre(x,y)
+        if x < 1 or x > 6:
+            libre = False
+        if y < 1 or y > 6:
+            libre = False
+            if y == 7 and x == 3:
+                libre = True
+        return libre
     def mover(self,direction):
         if self.orientation == "H":
             self.x = self.x + direction
--- main.py
+++ main.py
@@ -3,9 +3,45 @@
 
 import coche
 
+
+def in_game(n_nivel):
+    j_nivel = niveles[n_nivel - 1][:]
+    win = False
+    movimientos = 0
+    while not win:
+        # MOSTRAR RECORD
+        print "NIVEL",n_nivel -1,"- RECORD",( records[n_nivel - 1] if len(records) >= n_nivel else "SIN BATIR")
+        #pintar_nivel(j_nivel)
+        entrada = raw_input("Introduzca los movimientos: ")
+        for c in entrada:
+            for car in j_nivel:
+                if car.name == c.upper():
+                    ## COMPROBAR QUE EL MOVIMIENTO PUEDA SER POSIBLE
+                    direccion = 1 if c.lower() == c else -1
+                    if car.casilla_libre(direccion,j_nivel):
+                        car.mover(direccion)
+                        movimientos += 1
+                    else:
+                        print "Movimiento",c,"bloqueado"
+            ## COMPROBAR CONDICIÓN DE VICTORIA
+            if j_nivel[0].x == 3 and j_nivel[0].y == 7:
+                win = True
+    ## GUARDAR RECORD
+    print "Enhorabuena, has ganado"
+    records[0] = movimientos
+    write_records()
+    
+        
+
 def play_menu():
     """ Muestra el menú de selección de nivel """
-
+    max_level = (len(records) + 1)
+    n_nivel = -1
+    while n_nivel < 1 or n_nivel > max_level:
+        n_nivel = raw_input("Elige el nivel a jugar (1-%d)" % max_level)
+        n_nivel = int(n_nivel)
+    print "Jugando nivel", n_nivel
+    in_game(n_nivel)
     return None
 
 def write_records():
@@ -14,11 +50,13 @@ def write_records():
 		f_records.write(str(record))
 		f_records.write("\n")
 def read_records():
+    try:
 	f_records = open("records.txt","r")
 	for line in f_records:
 		records.append(int(line))
-	print records		
-		
+	print records
+    except:
+        pass
 
 print "Anrokku"
 print "="*80
@@ -34,14 +72,17 @@ niveles = []
 f_niveles = open("niveles.txt","r")
 n_niveles = f_niveles.readline()
 n_niveles = int(n_niveles)
+names = "ABCDEFQHIJQLMNOPQRSTUVWXYZ"
 
 for i in range(0,n_niveles):
 	n_coches = int(f_niveles.readline())
 	coches = []
 	for j in range(0,n_coches):
-		coches.append(coche.Coche(f_niveles.readline()))
+		coches.append(coche.Coche(f_niveles.readline(),names[j]))
 	niveles.append(coches[:])
 
 play_menu()
+#pintar_nivel(niveles[0])
+
 
 print "Gracias por jugar"
